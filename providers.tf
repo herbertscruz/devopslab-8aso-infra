0624112e76307a0bca3b84e896de8dc2611bdcaa
Herberts da Silva Cruz Fortuna <herbertscruz@gmail.com>
2022-10-07 17:53:29 -0300

    Adjustment

--- providers.tf
+++ providers.tf
@@ -10,7 +10,7 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/vagrant/gcpkey.json")
+  credentials = file("/home/herbertsfortuna/gcpkey.json")
 
   project = "teste-351601"
   region  = "us-west1"
