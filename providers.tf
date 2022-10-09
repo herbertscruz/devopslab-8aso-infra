58203df3284444fef910dbf261381b11a7d8fdae
Herberts da Silva Cruz Fortuna <herbertscruz@gmail.com>
2022-10-09 17:04:43 -0300

    Credentials Removed

--- providers.tf
+++ providers.tf
@@ -9,9 +9,6 @@ terraform {
 
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
-
-  credentials = file("/home/herbertsfortuna/gcpkey.json")
-
   project = "lab-devops-cloud-8aso-herberts"
   region  = "us-west1"
   zone    = "us-west1-b"
